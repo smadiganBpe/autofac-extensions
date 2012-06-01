0b2a84ec631c126b426030681639316a86bb5c74
smadigan <smadigan@bridgepointeducation.com>
2012-06-01 06:43:33 -0700

    add customextensions.cs

---
+++ customextensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constellation.Common.Core.Extensions
+{
+	public static class CustomExtensions
+	{
+		public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+		{
+			source.ThrowIfNull("source");
+			action.ThrowIfNull("action");
+			foreach (T element in source)
+			{
+				action(element);
+			}
+		}
+
+		public static void ThrowIfNull(this object source, string  message)
+		{
+			if (source == null) {
+				throw new ArgumentNullException(message);
+			}
+		}
+
+		public static DateTime ConvertFromUnixTimestamp(this double timestamp) {
+			var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+			return origin.AddSeconds(timestamp);
+		}
+
+
+		public static double ConvertToUnixTimestamp(this DateTime date) {
+			var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+			var diff = date - origin;
+			return Math.Floor(diff.TotalSeconds);
+		}
+	}
+}
